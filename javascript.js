966444859d4bc70ba333043b703fa0d44ba0e8b2
srinija landa <srinijalanda@gmail.com>
2024-07-10 02:27:12 +0530

    v2

--- javascript.js
+++ javascript.js
@@ -1,12 +1,40 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const schoolDropdown = document.getElementById('school-dropdown');
-    const dropdownContent = document.getElementById('dropdown-content');
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log(event);
+  const schoolDropdown1 = document.getElementById("school-dropdown-1");
+  const dropdownContent1 = document.getElementById("dropdown-content-1");
+  const schoolDropdown2 = document.getElementById("school-dropdown-2");
+  const dropdownContent2 = document.getElementById("dropdown-content-2");
 
-    schoolDropdown.addEventListener('mouseenter', () => {
-        dropdownContent.style.display = 'block';
-    });
+  schoolDropdown1.addEventListener("mouseenter", () => {
+    dropdownContent1.style.display = "block";
+  });
 
-    schoolDropdown.addEventListener('mouseleave', () => {
-        dropdownContent.style.display = 'none';
-    });
+  schoolDropdown1.addEventListener("mouseleave", () => {
+    dropdownContent1.style.display = "none";
+  });
+
+  schoolDropdown2.addEventListener("mouseenter", () => {
+    dropdownContent2.style.display = "block";
+  });
+
+  schoolDropdown2.addEventListener("mouseleave", () => {
+    dropdownContent2.style.display = "none";
+  });
+  const h1Tag = document.querySelector("#info>h1");
+  const h2Tag = document.querySelector("#info>h2");
+  h1Tag.addEventListener("mouseenter", () => {
+    //add the transition with link icon
+    h1Tag.classList.add("hovered");
+  });
+  h2Tag.addEventListener("mouseenter", () => {
+    h2Tag.classList.add("hovered");
+  });
+
+  h1Tag.addEventListener("mouseleave", () => {
+    //remove the transition with link icon
+    h1Tag.classList.remove("hovered");
+  });
+  h2Tag.addEventListener("mouseleave", () => {
+    h2Tag.classList.remove("hovered");
+  });
 });
